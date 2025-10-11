1104bcafe929afea8576499aae7284eb889fccf9
André Diego Piske <andrepiske@gmail.com>
2025-10-11 11:48:52 -0300

    start something new

---
+++ lib/lima.rb
@@ -0,0 +1,5 @@
+
+module Lima
+end
+
+require 'lima/vm'
---
+++ lib/lima/vm.rb
@@ -0,0 +1,29 @@
+
+class Lima::VM
+  attr_accessor :name
+  attr_accessor :cpus
+  attr_accessor :memory
+  attr_accessor :driver
+  attr_accessor :arch
+
+  def initialize
+    @driver = 'vz'
+    @arch = 'aarch64'
+
+    @template = "template://_images/ubuntu-24.04"
+  end
+
+  def start_command
+    [
+      "limactl", "start",
+      "-y", "--log-format=json",
+      "--name=#{@name}",
+      "--arch=#{@arch}",
+      "--cpus=#{@cpus}",
+      "--memory=#{@memory}",
+      "--vm-type=#{@driver}",
+      "--network=lima:shared",
+      @template,
+    ]
+  end
+end
